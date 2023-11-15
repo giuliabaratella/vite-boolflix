97f40c72e9326cee52bd80701035cea6df242fec
giuliabaratella <giuliabaratella7@gmail.com>
2023-11-15 14:27:03 +0100

    imported Api info + checked data results

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,35 @@
 <template>
-  <div>
+  <section id="movies"></section>
+  <section id="series"></section>
 
-  </div>
 </template>
 
 <script>
+import axios from 'axios';
+import {store} from './assets/data/store'
   export default {
-    
+    data(){
+      return{
+          store,
+      }
+    },
+    methods:{
+      getMoviesandSeries(){
+        const movieUrl = store.urlApi + store.endpoint.movie;
+        axios.get(movieUrl, {params: this.store.params}).then ((resp)=> {
+          store.moviesList = resp.data.results;
+        })
+        const seriesUrl = store.urlApi + store.endpoint.series;
+        axios.get(seriesUrl, {params: this.store.params}).then ((resp)=> {
+          store.seriesList = resp.data.results;
+        })
+        console.log(store.moviesList);
+        console.log(store.seriesList);
+      },
+    },
+    created(){
+      this.getMoviesandSeries();
+    },
   }
 </script>
 
--- src/assets/data/store.js
+++ src/assets/data/store.js
@@ -1,4 +1,16 @@
 import {reactive} from 'vue';
 export const store= reactive ({
+    urlApi: 'https://api.themoviedb.org/3',
+    urlImg: 'https://image.tmdb.org/t/p/w500',
+    endpoint: {
+        movie: '/search/movie',
+        series: '/search/tv',
+    },
+    params:{
+    api_key: 'bd3bc1f46c81d86cf1ba39c68386e73e',
+    query:'a',
+    },
+    moviesList:[],
+    seriesList:[],
     
 })
